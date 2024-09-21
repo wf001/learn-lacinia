263538d698312959caf174d1576ef49004df408a
wf001 <wf001@diax.xyz>
2024-09-21 13:14:19 +0900

    remove memorized->camelCaseString conifg

--- backend/lacinia/src/sakilaapi/router.clj
+++ backend/lacinia/src/sakilaapi/router.clj
@@ -16,18 +16,9 @@
       (assoc :proxy true)))
 
 
-(def ^:private memoized->camelCaseString
-  "実装上kebab-case keywordでやっているものをJSONにするときにcamelCaseにしたい。
-   バリエーションはそれほどないはずなのでキャッシュする"
-  (clj.memo/lru csk.core/->camelCaseString {} :lru/threshold 1024))
-
-
 (def ^:private muuntaja-custom-config
   "https://cljdoc.org/d/metosin/muuntaja/0.6.8/doc/configuration"
   (-> muu.core/default-options
-      ;; JSONにencodeする時にキーをcamelCaseにする
-      (assoc-in [:formats "application/json" :encoder-opts]
-                {:encode-key-fn memoized->camelCaseString})
       ;; JSON以外のacceptでリクエストされたときに返らないように制限する
       (update :formats #(select-keys % ["application/json"]))
       muu.core/create))
