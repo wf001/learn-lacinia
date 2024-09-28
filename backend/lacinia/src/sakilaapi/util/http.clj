7f461f57a14ab50f2c90f0bc0c5cbc1c6b414d04
wf001 <wf001@diax.xyz>
2024-09-28 10:03:29 +0900

    add not found utility

--- backend/lacinia/src/sakilaapi/util/http.clj
+++ backend/lacinia/src/sakilaapi/util/http.clj
@@ -6,3 +6,8 @@
 (defn internal-server-error
   ([] (internal-server-error {:message "Internal server error"}))
   ([body] (rg.u.http-res/internal-server-error body)))
+
+
+(defn not-found
+  ([] (not-found {:message "Not found"}))
+  ([body] (rg.u.http-res/not-found body)))
